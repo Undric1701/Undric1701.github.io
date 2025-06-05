ec4612dcc8edb00dcdbc52ebfceff562182cf5a4
Tretyakow Andrey(AT7) <andrew.tretyakow@gmail.com>
2025-06-05 14:11:53 +0300

    added first version of task#2(Platon's figures)

--- task2/mth.js
+++ task2/mth.js
@@ -158,17 +158,14 @@ export function MatrMulMatr(M1, M2) {
         M1.A[0][0] * M2.A[0][1] + M1.A[0][1] * M2.A[1][1] + M1.A[0][2] * M2.A[2][1] + M1.A[0][3] * M2.A[3][1],
         M1.A[0][0] * M2.A[0][2] + M1.A[0][1] * M2.A[1][2] + M1.A[0][2] * M2.A[2][2] + M1.A[0][3] * M2.A[3][2],
         M1.A[0][0] * M2.A[0][3] + M1.A[0][1] * M2.A[1][3] + M1.A[0][2] * M2.A[2][3] + M1.A[0][3] * M2.A[3][3],
-
         M1.A[1][0] * M2.A[0][0] + M1.A[1][1] * M2.A[1][0] + M1.A[1][2] * M2.A[2][0] + M1.A[1][3] * M2.A[3][0],
         M1.A[1][0] * M2.A[0][1] + M1.A[1][1] * M2.A[1][1] + M1.A[1][2] * M2.A[2][1] + M1.A[1][3] * M2.A[3][1],
         M1.A[1][0] * M2.A[0][2] + M1.A[1][1] * M2.A[1][2] + M1.A[1][2] * M2.A[2][2] + M1.A[1][3] * M2.A[3][2],
         M1.A[1][0] * M2.A[0][3] + M1.A[1][1] * M2.A[1][3] + M1.A[1][2] * M2.A[2][3] + M1.A[1][3] * M2.A[3][3],
-
         M1.A[2][0] * M2.A[0][0] + M1.A[2][1] * M2.A[1][0] + M1.A[2][2] * M2.A[2][0] + M1.A[2][3] * M2.A[3][0],
         M1.A[2][0] * M2.A[0][1] + M1.A[2][1] * M2.A[1][1] + M1.A[2][2] * M2.A[2][1] + M1.A[2][3] * M2.A[3][1],
         M1.A[2][0] * M2.A[0][2] + M1.A[2][1] * M2.A[1][2] + M1.A[2][2] * M2.A[2][2] + M1.A[2][3] * M2.A[3][2],
         M1.A[2][0] * M2.A[0][3] + M1.A[2][1] * M2.A[1][3] + M1.A[2][2] * M2.A[2][3] + M1.A[2][3] * M2.A[3][3],
-
         M1.A[3][0] * M2.A[0][0] + M1.A[3][1] * M2.A[1][0] + M1.A[3][2] * M2.A[2][0] + M1.A[3][3] * M2.A[3][0],
         M1.A[3][0] * M2.A[0][1] + M1.A[3][1] * M2.A[1][1] + M1.A[3][2] * M2.A[2][1] + M1.A[3][3] * M2.A[3][1],
         M1.A[3][0] * M2.A[0][2] + M1.A[3][1] * M2.A[1][2] + M1.A[3][2] * M2.A[2][2] + M1.A[3][3] * M2.A[3][2],
@@ -203,7 +200,7 @@ export function Vec3DotVec3(V1, V2) {
     return V1.X * V2.X + V1.Y * V2.Y + V1.Z * V2.Z;
 }
 export function Vec3CrossVec3(V1, V2) {
-    return new VEC3(-V1.Z * V2.Y + V1.Y * V2.Z, -V1.X * V2.Z + V1.Z * V2.X, -V1.Y * V2.X + V1.X * V2.Y);
+    return new VEC3(V1.Y * V2.Z - V1.Z * V2.Y, V1.Z * V2.X - V1.X * V2.Z, V1.X * V2.Y - V1.Y * V2.X);
 }
 export function Vec3Len(V) {
     let len = Vec3DotVec3(V, V);
@@ -216,12 +213,22 @@ export function Vec3Len2(V) {
     return len;
 }
 export function Vec3Normalize(V) {
-    let len = Vec3Len(V);
+    let len = Vec3DotVec3(V, V);
 
     if (len != 0 && len != 1)
-        return Vec3DivNum(V, len);
+        return Vec3DivNum(V, Math.sqrt(len));
     else
         return V;
+}
+export function PointTransform(V, M) {
+    return Vec3Set(V.X * M.A[0][0] + V.Y * M.A[1][0] + V.Z * M.A[2][0] + M.A[3][0],
+        V.X * M.A[0][1] + V.Y * M.A[1][1] + V.Z * M.A[2][1] + M.A[3][1],
+        V.X * M.A[0][2] + V.Y * M.A[1][2] + V.Z * M.A[2][2] + M.A[3][2]);
+}
+export function VectorTransform(V, M) {
+    return Vec3Set(V.X * M.A[0][0] + V.Y * M.A[1][0] + V.Z * M.A[2][0],
+        V.X * M.A[0][1] + V.Y * M.A[1][1] + V.Z * M.A[2][1],
+        V.X * M.A[0][2] + V.Y * M.A[1][2] + V.Z * M.A[2][2]);
 }
 export function Vec3MulMatr(V, M) {
     let w = V.X * M.A[0][3] + V.Y * M.A[1][3] + V.Z * M.A[2][3] + M.A[3][3];
@@ -234,7 +241,7 @@ export function Vec3MulMatr(V, M) {
 export function MatrView(Loc, At, Up1) {
     let Dir = Vec3Normalize(Vec3SubVec3(At, Loc));
     let Right = Vec3Normalize(Vec3CrossVec3(Dir, Up1));
-    let Up = Vec3CrossVec3(Right, Dir);
+    let Up = Vec3Normalize(Vec3CrossVec3(Right, Dir));
 
     let m = new MATR(Right.X, Up.X, -Dir.X, 0,
         Right.Y, Up.Y, -Dir.Y, 0,
